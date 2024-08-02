75f62d060dd8b5995479e8f26722032f6ecd510a
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-02 12:21:50 +0530

    Update elif.py

--- elif.py
+++ elif.py
@@ -8,3 +8,11 @@ else:
         print("The number is negative.")
     else:
         print("The number is zero.")
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    print("*" * i)
+
+for i in range(n - 1, 0, -1):
+    print("*" * i)
