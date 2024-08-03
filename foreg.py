db67011fc1743fb345fcf3c6fb16f504ef20dc46
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-03 11:39:30 +0530

    Create foreg.py

---
+++ foreg.py
@@ -0,0 +1,9 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Use a for loop to iterate over the list
+for num in numbers:
+    # Calculate the square of the current number
+    square = num ** 2
+    # Print the result
+    print(f"The square of {num} is {square}")
