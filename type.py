23a96cf3f4b156825156db3e7f4e63bdff6a66c9
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-06 17:51:28 +0530

    Update type.py

--- type.py
+++ type.py
@@ -2,3 +2,33 @@ a = input("Enter something: ")
 print(type(a))
 name = input("What is your name? ")
 print("Hello, ", name)
+# Type casting in python 
+# Integer to Float
+int_num = 10
+float_num = float(int_num)
+print("Integer to Float:", float_num)
+
+# Float to Integer
+float_num = 10.5
+int_num = int(float_num)
+print("Float to Integer:", int_num)
+
+# String to Integer
+str_num = "10"
+int_num = int(str_num)
+print("String to Integer:", int_num)
+
+# String to Float
+str_num = "10.5"
+float_num = float(str_num)
+print("String to Float:", float_num)
+
+# Integer to String
+int_num = 10
+str_num = str(int_num)
+print("Integer to String:", str_num)
+
+# Float to String
+float_num = 10.5
+str_num = str(float_num)
+print("Float to String:", str_num)
