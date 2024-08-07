94c07122320391742ec8b6668ccb9703b02abf7a
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-07 18:03:37 +0530

    Create split_join.py

---
+++ split_join.py
@@ -0,0 +1,16 @@
+
+my_string = "Hello,World,This,Is,Python"
+
+
+words = my_string.split(",")
+
+
+print("Split string:")
+print(words)
+
+
+joined_string = "-".join(words)
+
+
+print("\nJoined string:")
+print(joined_string)
