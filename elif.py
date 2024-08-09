fe7b40f0cff103024e3ddcf02c0b15945dec5456
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-09 21:28:47 +0530

    Update elif.py

--- elif.py
+++ elif.py
@@ -16,3 +16,22 @@ for i in range(1, n + 1):
 
 for i in range(n - 1, 0, -1):
     print("*" * i)
+
+
+
+
+x = int(input("Enter a number: "))
+
+if x > 0:
+    if x % 2 == 0:
+        print("The number is positive and even.")
+    else:
+        print("The number is positive and odd.")
+else:
+    if x == 0:
+        print("The number is zero.")
+    else:
+        if x % 2 == 0:
+            print("The number is negative and even.")
+        else:
+            print("The number is negative and odd.")
