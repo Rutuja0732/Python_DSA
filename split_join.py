1070ace4e5e17c07e0c87823755d5b15ef93d728
Rutuja Ingale <rutujaingale059@gmail.com>
2024-08-11 12:31:22 +0530

    Update split_join.py

--- split_join.py
+++ split_join.py
@@ -23,3 +23,15 @@ print(comma_separated)  # Output: "1,2,3,4,5"
 names = ["John", "Mary", "Jane", "Bob"]
 dash_separated = "-".join(names)
 print(dash_separated)  # Output: "John-Mary-Jane-Bob"s
+
+
+
+t = int(input())
+
+for i in range(t):
+    input_list = input().split()
+    X, Y = map(int, input_list)
+    total_fees = X * Y
+    output_list = [str(total_fees)]
+    output_string = ' '.join(output_list)
+    print(output_string)
